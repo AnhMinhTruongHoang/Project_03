0e4bd2cf0f4c5d40b69057e1103386d8d86fe4cd
tankhanh <dinhtankhanh14@gmail.com>
2025-11-09 23:55:12 +0700

    Thống kê

--- BE/src/modules/orders/orders.module.ts
+++ BE/src/modules/orders/orders.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schemas';
+import { Address, AddressSchema } from '../location/schemas/address.schema';
+import { District, DistrictSchema } from '../location/schemas/district.schema';
+import { Province, ProvinceSchema } from '../location/schemas/province.schema';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+      { name: Address.name, schema: AddressSchema },
+      { name: District.name, schema: DistrictSchema },
+      { name: Province.name, schema: ProvinceSchema },
+    ]),
   ],
   controllers: [OrdersController],
   providers: [OrdersService],
